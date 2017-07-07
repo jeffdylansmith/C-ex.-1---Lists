948da9e7f5295cf22f1c9a10fe783c5643e0346b
Dylan Smith <jeffdylansmith@gmail.com>
2017-07-07 14:15:52 -0500

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+
+namespace lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> planetList = new List<string>(){"Mercury", "Mars"};
+            planetList.Add("Jupiter");
+            planetList.Add("Saturn");
+
+            List<string> outerPlanetList = new List<string>(){"Uranus", "Neptune"};
+            planetList.AddRange(outerPlanetList);
+            planetList.Insert(1, "Venus");
+            planetList.Insert(2, "Earth");
+            planetList.Add("Pluto");
+            string[] output = planetList.GetRange(0, 4).ToArray();
+             Console.WriteLine();
+             planetList.Remove("Pluto");
+            foreach( string planet in planetList)
+            {
+                Console.WriteLine(planet);
+            }
+        }
+    }
+}
+        
+    
+
